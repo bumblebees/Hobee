86d89249c807d2bd7fe6ca7b906a80b18bb8657c
elkbot <elkbot@users.noreply.github.com>
2016-11-10 18:53:39 +0100

    first try of sending events to MQTT

--- app/src/main/java/bumblebees/hobee/NewEventActivity.java
+++ app/src/main/java/bumblebees/hobee/NewEventActivity.java
@@ -2,6 +2,8 @@ package bumblebees.hobee;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -13,8 +15,6 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.Calendar;
-import java.util.Date;
-
 import bumblebees.hobee.utilities.MQTT;
 
 public class NewEventActivity extends AppCompatActivity {
@@ -26,7 +26,9 @@ public class NewEventActivity extends AppCompatActivity {
     TextView inputEventDescription;
     TextView inputEventDate;
     TextView inputEventTime;
+    TextView inputEventNumber;
     Spinner inputEventGender;
+    Spinner eventHobbyChoice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,10 +42,19 @@ public class NewEventActivity extends AppCompatActivity {
         inputEventDate = (TextView) findViewById(R.id.inputEventDate);
         inputEventTime = (TextView) findViewById(R.id.inputEventTime);
         inputEventGender= (Spinner) findViewById(R.id.inputEventGender);
+        inputEventNumber = (TextView) findViewById(R.id.inputEventNumber);
+        eventHobbyChoice = (Spinner) findViewById(R.id.eventHobbyChoice);
+
 
         //set gender spinner options
-        ArrayAdapter<String> genderChoice = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, new String[]{"any gender", "male", "female"});
+        ArrayAdapter<String> genderChoice = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, new String[]{"any gender", "male", "female"});
         inputEventGender.setAdapter(genderChoice);
+
+        //TODO: get these from your currently available hobbies
+        String[] hobbyChoices = {"basketball", "football", "fishing", "cooking"};
+        ArrayAdapter<String> hobbyChoice = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, hobbyChoices);
+
+        eventHobbyChoice.setAdapter(hobbyChoice);
 
         btnAddEvent = (Button) findViewById(R.id.eventAddNew);
 
@@ -70,9 +81,30 @@ public class NewEventActivity extends AppCompatActivity {
     }
 
     /**
+     * Generates a hash to be used as ID for MQTT topics.
+     * Current implementation hashes a  string combination of useID+timestamp
+     * @param id name of the event
+     * @param time UNIX timestamp when the event was created
+     * @return hash
+     */
+    //TODO:figure out a better way to implement this so that it is for sure unique and easy to generate
+    public String generateHash(String id, long time){
+        String hash;
+        String toBeHashed = id+time;
+        hash = Base64.encodeToString(String.valueOf(toBeHashed.hashCode()).getBytes(), Base64.URL_SAFE);
+        return hash;
+    }
+
+
+    /**
      * Creates the JSON that will be sent over MQTT using the completed fields in the form.
      */
     public void addNewEvent(){
+        long timeCreated = Calendar.getInstance().getTimeInMillis() / 1000L;
+        String eventCategory = eventHobbyChoice.getSelectedItem().toString();
+        String hostID = "121431535165141"; //temporary value
+        String hash = generateHash(hostID, timeCreated);
+
         JSONObject event = new JSONObject();
         JSONObject host = new JSONObject();
         JSONObject eventDetails = new JSONObject();
@@ -88,21 +120,24 @@ public class NewEventActivity extends AppCompatActivity {
             eventDetails.put("date", inputEventDate.getText());
             eventDetails.put("gender", inputEventGender.getSelectedItem().toString());
             eventDetails.put("description", inputEventName.getText());
+            eventDetails.put("maximum_people", inputEventNumber.getText());
 
             event.put("host", host);
+            event.put("category", eventHobbyChoice.getSelectedItem().toString());
             event.put("event", eventDetails);
-            //get the current time as a UNIX timestamp
-            long dateCreated = System.currentTimeMillis() / 1000L;
+            event.put("createdTime", timeCreated);
+            event.put("eventID", hash);
 
-            event.put("date_created", dateCreated);
 
 
         } catch (JSONException e) {
             e.printStackTrace();
         }
 
+
         MqttMessage msg = new MqttMessage(event.toString().getBytes());
-        String topic = "hobby/test2";
+        String topic = "hobby/event/"+eventCategory+"/"+hash;
+        Log.d("mqtt", topic);
         MQTT.getInstance().publishMessage(topic, msg);
 
     }
