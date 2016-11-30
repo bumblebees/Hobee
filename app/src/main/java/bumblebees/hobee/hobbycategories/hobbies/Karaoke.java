c5946bb0b054edae51a73f01e2a20e8f86eb04f0
Amanda <gushofam@student.gu.se>
2016-11-30 14:06:02 +0100

    adding constructors and fields to hobbies classes

--- app/src/main/java/bumblebees/hobee/hobbycategories/hobbies/Karaoke.java
+++ app/src/main/java/bumblebees/hobee/hobbycategories/hobbies/Karaoke.java
@@ -1,8 +1,14 @@
 package bumblebees.hobee.hobbycategories.hobbies;
 
+import bumblebees.hobee.objects.Hobby;
+
 /**
  * Created by amandahoffstrom on 2016-11-30.
  */
 
-public class Karaoke {
+public class Karaoke extends Hobby {
+
+    public Karaoke(double id, String name){
+        super(3.1, "Karaoke");
+    }
 }
